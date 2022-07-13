c598f9d2ccfadd96ebe9698ba489cb3e08fe74ff
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-07-13 18:02:47 +0100

    Added API

---
+++ app/api/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+from . import settings
+from .auth import auth_route, manager
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.include_router(auth_route, prefix="/auth")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def homepage(request: Request):
+    return settings.templates.TemplateResponse(
+        "pages/overview.html", {"request": request}
+    )
+
+
+@app.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return settings.templates.TemplateResponse("pages/login.html", {"request": request})
---
+++ app/static/client.js
@@ -0,0 +1,10 @@
+class Client {
+    // == Navigation ==
+    static go_to_homepage() {
+        window.location.href = "/";
+    }
+
+    static go_to_login() {
+        window.location.href = "/login";
+    }
+}
