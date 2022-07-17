1f3e510f95c0972bfcb98b955b69628c5147155f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-07-17 13:27:26 +0100

    Added bomb query methods to TileMap

--- plugins/board_plugin/src/resources/tile_map.rs
+++ plugins/board_plugin/src/resources/tile_map.rs
@@ -1,6 +1,21 @@
 use ndarray::Array2;
 
-use crate::resources::tile::Tile;
+use crate::{components::Coordinates, resources::tile::Tile};
+
+/// Delta coordinates for all 8 square neighbors
+/// [6] [7] [8]
+/// [4]     [5]
+/// [1] [2] [3]
+const SQUARE_COORDINATES: [(i8, i8); 8] = [
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (-1, 0),
+    (1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+];
 
 /// Base tile map.
 #[derive(Debug, Clone)]
@@ -44,5 +59,29 @@ impl TileMap {
 
     pub fn bomb_count(&self) -> u16 {
         self.bomb_count
+    }
+
+    pub fn safe_square_at(&self, coordinates: Coordinates) -> impl Iterator<Item = Coordinates> {
+        SQUARE_COORDINATES
+            .iter()
+            .copied()
+            .map(move |tuple| coordinates + tuple)
+    }
+
+    pub fn is_bomb_at(&self, coordinates: Coordinates) -> bool {
+        if coordinates.x >= self.width() || coordinates.y >= self.height() {
+            return false;
+        };
+        self.map[(coordinates.y as usize, coordinates.x as usize)].is_bomb()
+    }
+
+    pub fn bomb_count_at(&self, coordinates: Coordinates) -> u8 {
+        if self.is_bomb_at(coordinates) {
+            return 0;
+        }
+
+        self.safe_square_at(coordinates)
+            .filter(|coord| self.is_bomb_at(*coord))
+            .count() as u8
     }
 }
