c1da81f7c040c99da460a8bc5be0cc780745b388
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-07-20 18:47:16 +0100

    Added BoardAssets

--- plugins/board_plugin/src/resources/mod.rs
+++ plugins/board_plugin/src/resources/mod.rs
@@ -1,9 +1,11 @@
 pub use board::Board;
+pub use board_assets::*;
 pub use board_options::{BoardOptions, BoardPosition, TileSize};
 pub use tile::Tile;
 pub use tile_map::TileMap;
 
 mod board;
+mod board_assets;
 mod board_options;
 mod tile;
 mod tile_map;
