84eacda1170530869816607adb14771388e3811e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-07-21 01:14:10 +0100

    Added respawn timeout

--- app/static/wasm/snippets/fw-d6e837206972cd23/src/bin/main.js
+++ app/static/wasm/snippets/fw-d6e837206972cd23/src/bin/main.js
@@ -1,5 +1,8 @@
 export function alert_game_over() {
     console.log("GAME OVER!");
     $("#display").fadeOut();
+    setTimeout(() => {
+        location.reload();
+    }, 500);
     return "Rust";
 }
--- src/bin/main.js
+++ src/bin/main.js
@@ -1,6 +1,8 @@
 export function alert_game_over() {
     console.log("GAME OVER!");
     $("#display").fadeOut();
-    location.reload();
+    setTimeout(function () {
+        location.reload();
+    }, 1000);
     return "Rust";
 }
